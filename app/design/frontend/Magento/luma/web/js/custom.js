ec0ea1c8c1efc190b0303ef48ea638ca088d1418
falgun1486 <68633430+falgun1486@users.noreply.github.com>
2020-10-07 03:03:34 +0530

    code commit

---
+++ app/design/frontend/Magento/luma/web/js/custom.js
@@ -0,0 +1,16 @@
+require(['jquery'], function ( $ ) {
+
+$(document).ready(function () {
+
+   $(".block.newsletter .action.subscribe").click(function () {
+       alert("This is just a test");
+    });
+
+}); //end ready
+    
+});
+
+/*$( document ).ready(function() {
+     alert("This is just a test");
+});*/
+    
